bbccbddd3f413d3f1d5f3b445215ec9ff1fa2cc6
wise-anchor-7998 <garonhock@yahoo.com>
2019-09-13 14:37:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = [1]
+  element = 2 
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
