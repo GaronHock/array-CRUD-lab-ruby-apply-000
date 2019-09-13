db32967c281b585c23866903f6abb8b6b30cdc27
wise-anchor-7998 <garonhock@yahoo.com>
2019-09-13 14:45:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -30,13 +30,16 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array = [1, 2, "am"]
+  return array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array = ["wow"]
+  return array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array = ["hi","arrays"]
+  return array[-1]
 end
